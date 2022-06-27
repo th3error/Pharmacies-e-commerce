4b04b9a996c25ec2e05b14710ab02ef698fc37ee
Abdelrahman Moussa <the_eror001@hotmail.com>
2022-06-27 08:45:23 +0200

    add fillable columns

--- app/Models/ProductPharmacy.php
+++ app/Models/ProductPharmacy.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class ProductPharmacy extends Pivot
 {
-    //
+    protected $fillable = [
+        'product_id',
+        'pharmacy_id',
+        'price',
+        'quantity'
+    ];
 }
