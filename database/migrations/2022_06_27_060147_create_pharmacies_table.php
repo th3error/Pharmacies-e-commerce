538b1d8fe19b32bab5bd6629e4f5186ffbc8a691
Abdelrahman Moussa <the_eror001@hotmail.com>
2022-06-27 08:40:35 +0200

    add table columns

--- database/migrations/2022_06_27_060147_create_pharmacies_table.php
+++ database/migrations/2022_06_27_060147_create_pharmacies_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('pharmacies', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('address');
             $table->timestamps();
         });
     }
