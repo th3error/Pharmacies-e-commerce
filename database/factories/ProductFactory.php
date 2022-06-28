29aed8007eabba438fb4646bf2808d8a6af7f15d
Abdelrahman Moussa <the_eror001@hotmail.com>
2022-06-28 03:21:53 +0200

    add product factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Image;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,8 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title' => fake()->unique()->text(15),
+            'description' => fake()->sentence()
         ];
     }
 }
