0d1dad7f31b08999cb476fe574b860047115f8c8
Abdelrahman Moussa <the_eror001@hotmail.com>
2022-06-28 03:21:13 +0200

    add image factory

---
+++ database/factories/ImageFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
+ */
+class ImageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        static $number = 1;
+        return [
+            'product_id' => $number++,
+            'title' => fake()->word(),
+            'mime_type' => fake()->word(),
+            'alt' => fake()->word(),
+            'description' => fake()->sentence(),
+            'src' => fake()->url()
+        ];
+    }
+}
